a5517f6e2fa635ba2abf46efeadb819b7b03c2a9
Zhu Zhan Yan <program.nom@gmail.com>
2019-11-04 21:41:11 +0800

    Added bolierplate flask and flask sqlalchemy code

---
+++ src/backend/app.py
@@ -0,0 +1,24 @@
+#
+# Memento
+# Backend
+# Flask App
+#
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from . import config
+
+app = Flask(__name__)
+app.config.from_object(config)
+db = SQLAlchemy(app)
+
+# root route
+@app.route('/')
+def route_root():
+    return "Memento Backend API is up and running!"
+
+# health check route
+@app.route('/healthz')
+def healh_check():
+    return "OK"

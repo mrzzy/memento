347861abf5c7398bde164fd8856f0ad7efe77339
Zhu Zhan Yan <program.nom@gmail.com>
2019-11-07 14:11:39 +0800

    Added CRUD ops for assignment's event model & tests

--- src/backend/ops/assignment.py
+++ src/backend/ops/assignment.py
@@ -4,9 +4,10 @@
 # Assignment Ops
 #
 
+from datetime import datetime
+
 from ..app import db
 from ..models.assignment import *
-
 from .utils import map_dict, apply_bound
 
 ## Task Ops
@@ -14,6 +15,8 @@ from .utils import map_dict, apply_bound
 # pending - show only tasks that are currently incomplete 
 # author_id - show only tasks created by author with given user id 
 # do_by -show  only tasks that are due before given do_by datetime
+# skip - skip the first skip organisations
+# limit - output ids limit to the first limit organisations
 def query_tasks(pending=None, author_id=None, do_by=None, skip=0, limit=None):
     task_ids = Task.query.with_entities(Task.id)
     # apply filters
@@ -38,6 +41,7 @@ def get_task(task_id):
     mapping = [
         ("name", "name"),
         ("description", "description"),
+        ("duration", "duraiton"),
         ("deadline", "deadline"),
         ("completed", "completed"),
         ("author_id", "authorId")
@@ -88,3 +92,82 @@ def delete_task(task_id):
     db.session.delete(task)
     db.session.commit()
 
+## Event Ops
+# query ids of events
+# pending - show only pending events
+# author_id - show only events created by author with given user id
+# attend_by - show only events that have to be attended before and at attend_by
+# skip - skip the first skip organisations
+# limit - output ids limit to the first limit organisations
+def query_events(pending=None, author_id=None, attend_by=None, skip=0, limit=None):
+    # apply filters
+    event_ids = Event.query.with_entities(Event.id)
+    if not pending is None:
+        now = datetime.utcnow()
+        if pending == True:
+            event_ids = event_ids.filter(Event.start_time <= now)
+        else:
+            event_ids = event_ids.filter(Event.start_time > now)
+    if not author_id is None: event_id = event_ids.filter_by(author_id=author_id)
+    if not attend_by is None:
+        event_ids = event_ids.filter(Event.start_time <= attend_by)
+
+    # apply skip & limit
+    event_ids = [ i[0] for  i in event_ids ]
+    event_ids = apply_bound(event_ids, skip, limit)
+
+    return event_ids
+
+# get event for id
+# returns event as a dict 
+def get_event(event_id):
+    event = Event.query.get(event_id)
+    # map fields to dict
+    mapping = [
+        ("name", "name"),
+        ("description", "description"),
+        ("start_time", "startTime"),
+        ("duration", "duraiton"),
+        ("author_id", "authorId")
+    ]
+    return map_dict(event, mapping)
+
+# create a event
+# name - name of the event
+# description - description of the event, optional
+# start_time - the begin time of the event
+# duration - duration of event of event in seconds
+# author_id - user id of the author of the event
+# returns the id of the new event
+def create_event(name, start_time, duration, author_id, description=""):
+    event = Event(name=name, start_time=start_time, description=description,
+                  duration=duration, author_id=author_id)
+    db.session.add(event)
+    db.session.commit()
+
+    return event.id
+
+# update the event for the given event_id
+# name - name of the event
+# description - description of the event, optional
+# start_time - the begin time of the event
+# duration - duration of event of event in seconds
+# author_id - user id of the author of the event
+def update_event(event_id=None, name=None, start_time=None,
+                 duration=None, author_id=None, description=None):
+    event = Event.query.get(event_id)
+    if not name is None: event.name = name
+    if not start_time is None: event.start_time = start_time
+    if not duration is None: event.duration = duration
+    if not author_id is None: event.author_id = author_id
+    if not description is None: event.description = description
+    db.session.commit()
+
+# delete the event for the given event id
+# cascade deletes and dependent objects on event
+def delete_event(event_id):
+    # TODO: cascade delete
+
+    event = Event.query.get(event_id)
+    db.session.delete(event)
+    db.session.commit()
--- src/backend/tests/ops/assignment.py
+++ src/backend/tests/ops/assignment.py
@@ -45,3 +45,37 @@ class TestAssigmentOps(TestCase):
         delete_task(task_id)
         self.assertEqual(query_tasks(), [])
         delete_org(task_id)
+
+    def test_event_ops(self):
+        self.assertEqual(query_events(), [])
+
+        org_id = create_org("kompany")
+        manager_id = create_user(User.Kind.Supervisor,
+                              "John",
+                              "password",
+                              "john@jmail.com",
+                              org_id)
+        worker_id = create_user(User.Kind.Worker,
+                              "Joel",
+                              "password",
+                              "joel@jmail.com",
+                              org_id)
+        event_start_time = datetime.utcnow()
+        event_id = create_event("fishing trip",
+                              start_time=event_start_time,
+                              duration=60,
+                              author_id=manager_id)
+
+        event = get_event(event_id)
+        self.assertEqual(event["name"], "fishing trip")
+        self.assertEqual(query_events(), [event_id])
+        self.assertEqual(query_events(pending=False), [])
+        self.assertEqual(query_events(attend_by=event_start_time), [event_id])
+
+        update_event(event_id, name="cooking competition")
+        event = get_event(event_id)
+        self.assertEqual(event["name"], "cooking competition")
+
+        delete_event(event_id)
+        self.assertEqual(query_events(), [])
+        delete_org(event_id)

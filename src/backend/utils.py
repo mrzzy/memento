c0bd8fe0b870437e9da66b501ab27ad47199c4e9
Zhu Zhan Yan <program.nom@gmail.com>
2019-11-09 15:21:49 +0800

    Added map_obj() utility to map from dict to object

--- src/backend/utils.py
+++ src/backend/utils.py
@@ -4,6 +4,11 @@
 # Operations Utilites
 #
 
+# reverse mapping - swap mappings ordering
+# returns reverse mapping
+def swap_mapping(mapping):
+    return [ (a, b) for a, b in mapping ]
+
 # maps the given object to a dict using given mapping
 # mapping is a list of (field, key)
 # returns the mappped dictionary
@@ -12,6 +17,14 @@ def map_dict(obj, mapping):
     for field, key in mapping:
         obj_dict[key] = getattr(obj, field)
     return obj_dict
+
+# maps data from map_dict into object fields on the given object
+# using the given mapping (a list of (key, field))
+# returns the obj with the updated fields
+def map_obj(obj, map_dict, mapping):
+    for key, field in mapping:
+        setattr(obj, field, map_dict[key])
+    return obj
 
 # apply skip and limit to the given list of items
 # returns the updated list

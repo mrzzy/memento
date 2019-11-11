87af94f684be91b60b0186cf112d564bd49e88e1
Zhu Zhan Yan <program.nom@gmail.com>
2019-11-11 15:51:36 +0800

    Added CRUD API for teams

--- src/backend/api/iam.py
+++ src/backend/api/iam.py
@@ -9,7 +9,7 @@ import json
 from flask import request, Blueprint, jsonify
 from sqlalchemy.exc import IntegrityError
 
-from ..utils import map_keys, reverse_mapping
+from .utils import parse_params
 from ..mapping.iam import *
 from ..ops.iam import *
 from ..config import API_VERSION
@@ -33,24 +33,18 @@ def route_orgs():
 @iam.route(f"/api/v{API_VERSION}/iam/org", methods=["POST"])
 @iam.route(f"/api/v{API_VERSION}/iam/org/<org_id>", methods=["GET", "PATCH", "DELETE"])
 def route_org(org_id=None):
-    def parse_body():
-        # parse params in json
-        params = request.get_json()
-        params = map_keys(params, reverse_mapping(org_mapping))
-        return params
-
     if request.method == "GET" and org_id:
         # get org for id
         org = get_org(org_id)
         return jsonify(org)
     elif request.method == "POST" and request.is_json:
         # create org with params in json
-        params = parse_body()
+        params = parse_params(request, org_mapping)
         org_id = create_org(**params)
         return jsonify({ "id": org_id })
     elif request.method == "PATCH" and org_id and request.is_json:
         # parse params in json
-        params = parse_body()
+        params = parse_params(request, org_mapping)
         # update org with params in json
         update_org(org_id, **params)
         return jsonify({"success": True })
@@ -59,5 +53,44 @@ def route_org(org_id=None):
         delete_org(org_id)
         return jsonify({"success": True })
     else:
-        # malformed request
+        raise NotImplementedError
+
+## Team API
+# api - query teams with filters
+@iam.route(f"/api/v{API_VERSION}/iam/teams", methods=["GET"])
+def route_teams():
+    # parse query args
+    skip = int(request.args.get("skip", 0))
+    limit = request.args.get("limit", None)
+    if not limit is None: limit = int(limit)
+    org_id = request.args.get("org", None)
+
+    # query teams
+    team_ids = query_teams(org_id, skip, limit)
+    return jsonify(team_ids)
+
+# api - read, create, update, delete teams
+@iam.route(f"/api/v{API_VERSION}/iam/team", methods=["POST"])
+@iam.route(f"/api/v{API_VERSION}/iam/team/<team_id>", methods=["GET", "PATCH", "DELETE"])
+def route_team(team_id=None):
+    if request.method == "GET" and team_id:
+        # get team for id
+        team = get_team(team_id)
+        return jsonify(team)
+    elif request.method == "POST" and request.is_json:
+        # create team with params in json
+        params = parse_params(request, team_mapping)
+        team_id = create_team(**params)
+        return jsonify({ "id": team_id })
+    elif request.method == "PATCH" and team_id and request.is_json:
+        # parse params in json
+        params = parse_params(request, team_mapping)
+        # update team with params in json
+        update_team(team_id, **params)
+        return jsonify({"success": True })
+    elif request.method == "DELETE" and team_id:
+        # delete team with params in json
+        delete_team(team_id)
+        return jsonify({"success": True })
+    else:
         raise NotImplementedError

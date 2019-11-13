171a0c7f99d58c954489312aacd8eac4a963ee62
Zhu Zhan Yan <program.nom@gmail.com>
2019-11-13 15:46:57 +0800

    Fix CORS error when making api requests from another origin

--- src/backend/app.py
+++ src/backend/app.py
@@ -5,6 +5,7 @@
 #
 
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -15,6 +16,7 @@ app = Flask(__name__)
 app.config.from_object(config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+cors = CORS(app, allow_headers='Content-Type')
 
 # register api components
 from .api import register_api

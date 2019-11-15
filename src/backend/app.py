81cae92ffdc808c745451f9c57e3481d442aa02b
Zhu Zhan Yan <program.nom@gmail.com>
2019-11-15 21:16:53 +0800

    Configured backend database health check to ensure database is working

--- src/backend/app.py
+++ src/backend/app.py
@@ -9,6 +9,7 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_sockets import Sockets
+from healthcheck import HealthCheck
 
 from . import config
 
@@ -16,9 +17,11 @@ from . import config
 app = Flask(__name__)
 app.config.from_object(config)
 db = SQLAlchemy(app)
-starmigrate = Migrate(app, db)
+migrate = Migrate(app, db)
 cors = CORS(app, allow_headers='Content-Type')
 sockets = Sockets(app)
+# configure healthcheck route
+health = HealthCheck(app, "/healthz")
 
 # register api components
 from .api import register_api
@@ -30,7 +33,16 @@ register_api(app, sockets)
 def route_status():
     return "Memento Backend API is up and running!"
 
-# k8s health check route
-@app.route('/healthz')
-def health_check():
-    return "OK"
+## health checks
+# check database connectivity
+@health.add_check
+def check_database():
+    # try to execute a simple query on database
+    is_database_ok = True
+    message = "database ok"
+    try:
+        db.session.execute("SELECT 1")
+    except Exception as e:
+        message = str(e)
+        is_database_ok = False
+    return is_database_ok, message

cf6a905b6bc4b72580565875c7d9c5273339bb32
Zhu Zhan Yan <program.nom@gmail.com>
2019-11-15 15:43:33 +0800

    Added tests for subscription related notification ops

--- src/backend/tests/ops/notification.py
+++ src/backend/tests/ops/notification.py
@@ -4,8 +4,11 @@
 # Notification Ops Tests
 #
 
+import time
 from unittest import TestCase
 from datetime import datetime
+
+from gevent.lock import Semaphore
 
 from ...ops.iam import *
 from ...ops.notification import *
@@ -71,4 +74,26 @@ class TestsNotficationOps(TestCase):
 
         delete_notify(notify_id)
         self.assertEqual(query_notifys(), [])
+        delete_org(org_id)
+
+    def test_subscription(self):
+        org_id, user_id = self.create_test_data()
+        channel_id = create_channel(Channel.Kind.Notice, user_id)
+
+        # define a test handler to run on notificaion
+        run_lock = Semaphore()
+        def handler(subscribe_id, message):
+            self.assertTrue(run_lock.acquire())
+            notify = handle_notify(subscribe_id, message)
+            self.assertEqual(notify["title"], "fish")
+
+        subscribe_id = subscribe_channel(channel_id, handler)
+        notify_id = create_notify("fish", channel_id)
+        time.sleep(0.0001) # simulate blocking operation
+        self.assertTrue(run_lock.locked())
+
+        notify_id = create_notify("fish", channel_id)
+        time.sleep(0.0001) # simulate blocking operation
+
+        unsubscribe_channel(subscribe_id)
         delete_org(org_id)

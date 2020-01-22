66b70d88bdb7318ea47d15fa28422ae2836aba93
Zhu Zhanyan <program.nom@gmail.com>
2020-01-22 22:41:55 +0800

    Added API.js client for memento backend API & tests

---
+++ src/frontend/src/API.js
@@ -0,0 +1,258 @@
+/*
+ * Memento
+ * Frontend 
+ * API Client
+*/
+
+import assert from "assert";
+import fetch from "cross-fetch";
+import dotenv from "dotenv";
+import Cookie from "js-cookie";
+
+// defines an api client to interface with the backend api
+export default class API {
+    // constructs client to interface with the api
+    constructor() {
+        // load config from environment 
+        dotenv.config();
+        this.apiHost = process.env.BACKEND_API_HOST || "backend:5000";
+        this.apiVersion = 1
+        this.protocol = process.env.BACKEND_API_PROTOCOL || "http";
+        // check protocol support
+        assert(this.protocol === "http" || this.protocol === "https");
+        // build api root url from configuration
+        this.apiRoot = `${this.protocol}://${this.apiHost}/api/v${this.apiVersion}`
+
+        // object state
+        this.stateCookieName = "memento-api-obj-state";
+        // try to load state form cookie
+        this.state = Cookie.getJSON(this.stateCookieName);
+        if(this.state == null) {
+            this.state = { 
+                "refreshToken": null,
+                "accessToken": null,
+            };
+        }
+
+        // build map of available api routes
+        this.apiMap = {
+            "auth": ["login", "check", "refresh"],
+            "identity": 
+            [ "org", "user", "team", "team/assign", "manage", "role", "rolebind"],
+            "assignment": ["task", "event", "assign"],
+            "notify": ["notify", "channel"],
+        };
+    }
+
+    /* utils */
+    // convert the given set of url param to url param string
+    // returns the url param string 
+    convertUrl(params) {
+        return "?" + Object.entries(params)
+            .map(([key, value]) => `${key}=${value}`).join("&");
+    }
+
+    // returns true if given type is supported, otherwise false
+    supports(type)  {
+        for(let types of Object.values(this.apiMap)) {
+            if(types.includes(type)) {
+                return true
+            }
+        }
+        return false
+    }
+
+    // build object url 
+    // type - type of object to build. Must be supported
+    objectUrl(type) {
+        // check object type supported
+        assert(this.supports(type));
+        // map type to service
+        for(let [service, types] of Object.entries(this.apiMap)) {
+            if(types.includes(type)) {
+                // found matching service & type in api map 
+                const objectUrl = `${this.apiRoot}/${service}/${type}`;
+                return objectUrl;
+            }
+        }
+    
+        throw `API: Unknown api type: ${type}`;
+    }
+
+    // set the values in the given valueMap dict to the object state
+    // commits the state into a cookie for persistence
+    setState(valueMap) {
+        for(let [key, value] of Object.entries(valueMap)) {
+            this.state[key] = value;
+        }
+    
+        // store object state for 14 days
+        Cookie.set(this.stateCookieName, this.state, { "expires": 14 });
+    }
+
+    /* authentication */
+    // perform authentication with the given user credentials
+    // should be called before making most API calls
+    // username - username of the user 
+    // password - password of the user
+    // returns true if login success, false otherwise
+    async login(username, password) {
+        const response =  await this.post("login", {
+            "username": username,
+            "password": password
+        })
+        
+        if(response.refreshToken != null) {
+            this.setState({"refreshToken": response.refreshToken});
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    // attempts to refresh the access token using the refreshToken
+    // only refreshes the access token once it expires
+    // if no refreshToken is available does nothing
+    // return true if refresh success, false otherwise
+    async refresh() {
+        // check if has refreshToken to perfrom refresh 
+        if(this.state.refreshToken == null) return;
+        // check access token has expired
+        if(this.state.accessToken != null) return;
+        // make refresh request
+        var response = await fetch(this.objectUrl("refresh"), {
+            method: "GET",
+            mode: "cors",
+            cache: "no-cache",
+            headers: {
+                "Authorization": `Bearer ${this.state.refreshToken}`
+            }
+        });
+        response = await response.json();
+        
+        if(response.accessToken != null) {
+            this.setState({"accessToken": response.accessToken});
+            // auto expire access token
+            setTimeout(() => {
+                this.state.accessToken = nullj
+            }, 4 * 60 * 1000);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    // attach token to authenticate with the api server to the given request
+    // uses access token if available, request token if available, otherwise does nothing
+    // returns the given request with the authentication token attached
+    attachToken(request) {
+        // determine token to attach
+        let token = "";
+        if(this.state.accessToken != null) {
+            token = this.state.accessToken;
+        } else if(this.state.refreshToken != null) {
+            token = this.state.refreshToken;
+        }
+    
+        // attach token by setting auth header in request
+        if(token !== "") { 
+            const headers = ("headers" in request) ? request.headers : {};
+            headers["Authorization"] = `Bearer ${token}`;
+            request.headers = headers;
+        }
+
+        return request;
+    }
+    
+    
+    /* CRUD operations */
+    // query objects
+    // type - type of object to query
+    // params - query parameters to pass on query
+    async query(type, params={}) {
+        // build query url
+        const queryParams = this.convertUrl(params);
+        const queryUrl = `${this.objectUrl(type)}s${queryParams}`;
+        // perform query request
+        this.refresh();
+        const response = await fetch(queryUrl, this.attachToken({}));
+        return await response.json();
+    }
+
+    // get object
+    // type - type of object to get
+    // id - id of the object to get
+    async get(type, id) {
+        const objUrl = `${this.objectUrl(type)}/${id}`;
+    
+        // perform get request
+        this.refresh();
+        const response = await fetch(objUrl, this.attachToken({
+            method: "GET",
+            mode: "cors",
+            cache: "no-cache"
+        }));
+
+        return await response.json();
+    }
+
+    // post object
+    // type - type of object to create
+    // params - params to pass to create object
+    // returns api response
+    async post(type, params) {
+        // perform create request 
+        this.refresh();
+        const response = await fetch(this.objectUrl(type), this.attachToken({
+            method: "POST",
+            mode: "cors",
+            cache: "no-cache",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(params)
+        }));
+        
+        return await response.json();
+    }
+    
+    // update object
+    // type - type of object to update
+    // id - id of the object to update
+    // params - params to pass to update object
+    // returns api response
+    async update(type, id, params) {
+        const objUrl = `${this.objectUrl(type)}/${id}`;
+
+        // perform update request 
+        this.refresh();
+        const response = await fetch(objUrl, this.attachToken({
+            method: "PATCH",
+            mode: "cors",
+            cache: "no-cache",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(params)
+        }));
+        
+        return await response.json();
+    }
+
+    // delete object
+    // type - type of object to delete
+    // id - id of the object to delete 
+    // returns api response
+    async delete(type, id) {
+        // perform delete request
+        const objUrl = `${this.objectUrl(type)}/${id}`;
+        this.refresh();
+        const response = await fetch(objUrl, this.attachToken({
+            method: "DELETE",
+            mode: "cors",
+            cache: "no-cache"
+        }));
+        
+        return await response.json();
+    }
+}
---
+++ src/frontend/src/API.test.js
@@ -0,0 +1,86 @@
+
+/*
+ * Memento
+ * Frontend 
+ * API Client - Tests
+*/
+
+import API from "./API";
+import assert from "assert";
+
+describe("API", () => {
+    const api = new API(); 
+    let orgId;
+
+    // create required objects for testing
+    describe("#post(type, params)", () => {
+        it("should create organisation", async () => {
+            const response = await api.post("org", {
+                name: "James Bakery"
+            });
+            assert(response.id);
+            orgId = response.id
+            
+        });
+        it("should create user", async () => {
+            const response = await api.post("user", {
+                "name": "joel",
+                "password": "P@$$w0rd",
+                "email": "joel@gmail.com",
+                "orgId": orgId
+            })
+            assert(response.id);
+        });
+    });
+
+    // authentication
+    describe("#login(username, password)", () => {
+        it("show perform login", async () => {
+            const hasLogin = await api.login("joel@gmail.com", "P@$$w0rd");
+            assert(hasLogin);
+            assert(api.state.refreshToken);
+        });
+    });
+    
+    describe("#refresh(username, password)", () => {
+        it("show perform access token refresh", async () => {
+            const hasRefresh = await api.refresh();
+            assert(hasRefresh);
+        });
+    });
+    
+
+    
+    // test crud on auth api
+    describe("#query(type, params)", () => {
+        it("should get orgs ids", async () => {
+            const orgIds = await api.query("org", {"limit": 1, "skip": 0});
+            assert(orgIds.length === 1);
+        });
+    });
+    describe("#get(type, id)", () => {
+        it("should get organisation", async () => {
+            const orgIds = await api.query("org", {"limit": 1, "skip": 0});
+            const org = await api.get("org", orgIds[0]);
+            assert(org.name === "James Bakery");
+        });
+    });
+    describe("#update(type, id, params)", () => {
+        it("should update organisation", async () => {
+            const orgIds = await api.query("org", {"limit": 1, "skip": 0});
+            const response = await api.update("org", orgIds[0], {
+                name: "John's Bakery"
+            });
+            assert(response.success);
+        });
+    });
+    
+    // cleanup
+    describe("#delete(type,id)", () => {
+        it("should delete organisation", async() => {
+            const orgIds = await api.query("org", {"limit": 1, "skip": 0});
+            const response = await api.delete("org", orgIds[0]);
+            assert(response.success);
+        });
+    });
+});

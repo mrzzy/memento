12f5fa24c05284767a813b34c732458520603398
Zhu Zhanyan <program.nom@gmail.com>
2020-01-24 10:59:20 +0800

    Added APIHelpers.js to decorate API.js with helper functions

---
+++ src/frontend/src/APIHelpers.js
@@ -0,0 +1,43 @@
+/*
+ * Memento
+ * Frontend 
+ * API Helpers
+*/
+
+// defines a decorator that decorates the API object with various 
+// helper convience functions
+export default class APIHelpers {
+    // wrap & decorate the given api with helpers
+    constructor(api) {
+        this.api = api;
+    }
+        
+    // retrieve employee userIds for employer with the given employer id
+    // employerId - user id of the employee to create tasks for 
+    // returns user ids of employee of employer
+    getEmployeesForEmployer(employerId) {
+        // get managment relationships for employer
+        const manages =  this.api.query("manage", {"manager": employerId});
+        const employeeeIds = manages.map((manage) => manage.manageeId);
+    
+        return employeeeIds
+    }
+    
+    // get tasks assigned to userId
+    // userId - id of the employee to get tasks for
+    // return tasks
+    getTasks(userId) {
+        const taskIds = this.api.query("task", {"assignee": userId});
+        return taskIds
+    }
+
+    // get tasks assigned to userId for the given date
+    // userId - id of the employee to get tasks for
+    // date - UTC datetime specifying the day to get tasks
+    // returns task ids
+    getTasksForDate(userId, date) {
+        const taskIds = this.api.query("task", {"assignee": userId, "for-day": date});
+        return taskIds
+    }
+    
+}
